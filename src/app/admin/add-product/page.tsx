d5d79c9a146481780285ca153cc67d7264795393
mortomulu <mfajrihidayat09@gmail.com>
2024-05-31 23:25:23 +0700

    cleanup: add product page

--- src/app/admin/add-product/page.tsx
+++ src/app/admin/add-product/page.tsx
@@ -1,27 +1,8 @@
-// "use client"
-
-import FormAddProduct from "@/components/FormAddProduct/FormAddProduct";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 import { AddProduct } from "./components";
 
 const AddProductPage = () => {
-  // const router = useRouter();
-
-  // const { data: session, status } : {data : any, status: string} = useSession();
-
-  // console.log(status);
-
-  // useEffect(() => {
-  //   if (status === "unauthenticated" && session?.user.role !== 'admin') {
-  //     router.push("/");
-  //   }
-  // }, [router, status]);
-
   return (
     <div>
-      {/* <FormAddProduct /> */}
       <AddProduct />
     </div>
   );
