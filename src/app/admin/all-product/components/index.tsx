39c9f527caaa53b994b999ff73a9ff75ad67a3b9
mortomulu <mfajrihidayat09@gmail.com>
2024-05-31 23:17:58 +0700

    cleanup: all product page

--- src/app/admin/all-product/components/index.tsx
+++ src/app/admin/all-product/components/index.tsx
@@ -1,21 +1,7 @@
-// "use client";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 import AdminLayout from "@/components/layout/layoutAdmin";
 import TableAllProductContainer from "./tableAllProduct";
 
 export const AllProduct = ({ products }: any) => {
-//   console.log(products);
-//   const router = useRouter();
-
-//   const { data: session, status }: { data: any; status: string } = useSession();
-
-//   useEffect(() => {
-//     if (status === "unauthenticated" && session?.user.role !== "admin") {
-//       router.push("/");
-//     }
-//   }, [router, status]);
   return (
     <AdminLayout>
       <TableAllProductContainer products={products} />
