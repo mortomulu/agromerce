001c99a6fabe8e85c3f72d1f9d1b62496c7ee89a
mortomulu <mfajrihidayat09@gmail.com>
2024-05-31 21:16:15 +0700

    cleanup: landing page

--- src/app/components/products/index.tsx
+++ src/app/components/products/index.tsx
@@ -1,7 +1,6 @@
 import { getProducts } from "@/lib/crudProduct/dbData";
 import { Product } from "@/types/product";
-import ProductCard from "@/components/productCard/ProductCard";
-import Link from "next/link";
+import ProductCard from "./ProductCard";
 
 const Products = async () => {
   const products = await getProducts();
@@ -9,8 +8,7 @@ const Products = async () => {
   return (
     <div className="grid grid-cols-4 gap-4 mx-8 pb-8">
       {products?.map((product: Product) => (
-        
-          <ProductCard key={product.id} todo={product} />
+        <ProductCard key={product.id} todo={product} />
       ))}
     </div>
   );
