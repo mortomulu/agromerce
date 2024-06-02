b52e0c6ee260301455196bb923744f77585bc62f
mortomulu <mfajrihidayat09@gmail.com>
2024-06-02 13:53:38 +0700

    fix prod: refactor hit api

--- src/app/admin/all-product/page.tsx
+++ src/app/admin/all-product/page.tsx
@@ -1,18 +1,9 @@
 import { AllProduct } from "./components";
-import { getProducts } from "@/lib/crudProduct/dbData";
 
 const AllProductPage = async () => {
-  let products = [];
-  
-  try {
-    products = await getProducts();
-  } catch (error) {
-    console.error('Error fetching products:', error);
-  }
-
   return (
     <div>
-      <AllProduct products={products} />
+      <AllProduct />
     </div>
   );
 };
