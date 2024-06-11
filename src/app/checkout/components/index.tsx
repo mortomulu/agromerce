732b61b5b75184a2e5c51130f63a772035355e8b
mortomulu <mfajrihidayat09@gmail.com>
2024-06-11 17:30:52 +0700

    feat: checkout page

---
+++ src/app/checkout/components/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/layout/layout";
+
+const Checkout = () => {
+  return (
+    <Layout>
+      <div className="pt-20 min-h-screen">Checkout Page</div>
+    </Layout>
+  );
+};
+
+export default Checkout;
