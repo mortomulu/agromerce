1be9bcbafcbc8e7e877a228b1af3f5fb879eeed8
mortomulu <mfajrihidayat09@gmail.com>
2024-06-13 12:47:22 +0700

    fix prod: fetch cart server 3

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -4,21 +4,5 @@ import Cart from "./components";
 const url = process.env.NEXT_PUBLIC_BASE_URL;
 
 export default async function CartPage() {
-  try {
-    const response = await fetch(`${url}api/cart`, {
-      next: { revalidate: 10 },
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch cart data");
-    }
-
-    const data = await response.json();
-
-    return <Cart />;
-  } catch (error) {
-    console.error("Error fetching cart data:", error);
-    return <div>Error loading cart data</div>;
-  }
+  return <Cart />;
 }
-
