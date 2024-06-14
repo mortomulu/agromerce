cca224921416b59265350690fe31bafd4a654697
mortomulu <mfajrihidayat09@gmail.com>
2024-06-14 19:30:00 +0700

    fix prod: cancel fix cors policy block

--- next.config.mjs
+++ next.config.mjs
@@ -43,14 +43,6 @@ const nextConfig = {
       },
     ];
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://api.example.com/:path*',
-      },
-    ];
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
