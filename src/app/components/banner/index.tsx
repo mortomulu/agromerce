341bdca5d6ed3234e84afda3b5419f606fb4679a
Kelvin Dwipangga <144301420+klvnd@users.noreply.github.com>
2024-07-02 21:26:33 +0700

    update banner

--- src/app/components/banner/index.tsx
+++ src/app/components/banner/index.tsx
@@ -4,26 +4,38 @@ import Image from "next/image";
 const BannerLanding = () => {
   return (
     <div className="px-8 mb-8 pt-24">
-      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 overflow-hidden rounded-[32px]">
         <Carousel>
-          <Image
-            src="/banner-wellcome.svg"
-            alt="..."
-            width={100}
-            height={100}
-          />
-          <Image
-            src="/fitur-konsultasi.svg"
-            alt="..."
-            width={100}
-            height={100}
-          />
-          <Image
-            src="/fitur-komparasi.svg"
-            alt="..."
-            width={100}
-            height={100}
-          />
+          <div className="rounded-[32px] overflow-hidden">
+            <Image
+              src="/banner-wellcome.svg"
+              alt="..."
+              layout="responsive"
+              width={1700}
+              height={100}
+              className="rounded-[32px]"
+            />
+          </div>
+          <div className="rounded-[32px] overflow-hidden">
+            <Image
+              src="/fitur-konsultasi.svg"
+              alt="..."
+              layout="responsive"
+              width={1700}
+              height={100}
+              className="rounded-[32px]"
+            />
+          </div>
+          <div className="rounded-[32px] overflow-hidden">
+            <Image
+              src="/fitur-komparasi.svg"
+              alt="..."
+              layout="responsive"
+              width={1700}
+              height={100}
+              className="rounded-[32px]"
+            />
+          </div>
         </Carousel>
       </div>
     </div>
